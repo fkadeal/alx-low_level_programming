24b1291301e48a2ec47b4fd44354aa9c67b56d05
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-04 08:19:49 -0800

    print using print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
-#!/bin/bash
-	int main(void)
-	printf("with proper grammar, but the outcome is a piece of art,")
-	return 0
-
+#include<stdio.h>
+	int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+	}
