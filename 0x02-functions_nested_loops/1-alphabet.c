4db7eec60506fc2ae50f96a0a36c552f9a72e194
Fkadeal Matiwos <fkadeal@github.com>
2021-02-09 14:29:06 +0000

    add alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    for(char ch="a" ; ch<="z";ch++){
+	_putchar(ch);
+}
+   _putchar("\n");
+    return (0);
+}
