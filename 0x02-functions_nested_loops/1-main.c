c18d5d6f462435b184d6f5e07e40d9178feef43c
Fkadeal Matiwos <fkadeal@github.com>
2021-02-09 15:06:37 +0000

    added commpiler main.c file and modify

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,11 +5,8 @@
  *
  * Return: Always 0.
  */
-int print_alphabet(void)
+int main(void)
 {
-    for(char ch='a' ; ch<='z';ch++){
-	_putchar(ch);
-}
-   _putchar('\n');
-    
+    print_alphabet();
+    return (0);
 }
