235ba4d80e07d4aae6326daa094a7897b6a2fd99
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-09 20:11:41 -0800

    modify

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 
 /**
- * Return: the sum (int y, int x)
+ * Return the sum (int y, int x)
  */
 
 int add(int y, int x)
-return (y + x);
+{
+  int sum;
+  sum = y+x;
+return (sum);
+}
 
