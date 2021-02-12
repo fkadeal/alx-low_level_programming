e35126e03c4e502be5f07ab5ada2e03ead2aaaec
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-12 04:24:06 -0800

    crt 5-more_numbers.c file
    a function that prints 10 times the numbers, 
     * from 0 to 14, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * a function that prints 10 times the numbers, 
+ * from 0 to 14, followed by a new line.
+ */
+
+void more_numbers(void)
+{
+int i;
+int n;
+
+for (n = 1; n <= 10; n++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
