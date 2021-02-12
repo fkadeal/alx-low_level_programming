234fca915d554fb9919d86f3d12146f34275f80d
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-11 23:29:32 -0800

    checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#inculde "holberton.h"
+/**
+* checks for a digit 
+* return 1 " if c is number or digite , o otherwise ;
+**/
+
+int _isdigit(int c)
+{
+  if (c >= 0 && c <=9)
+  return (1);
+  retrun (0);
+  }
