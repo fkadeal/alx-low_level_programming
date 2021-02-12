937bc488da189b408db33913bd225b0622754649
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-12 07:57:42 -0800

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,27 @@
 #include "holberton.h"
+
 void print_triangle(int size)
 {
-  int a, b, c, col;
+int a, b, c, col;
 columns = size - 1;
 if (length > 0)
-    {
-      for (a = 0; a < size; a++)
-	{
-	  for (b = 0; b < col; b++)
-	    {
-	      _putchar(' ');
-	    }
-	  columns--;
-	  for (c = 0; c < (a + 1); c++)
-	    {
-	      _putchar('#');
-	    }
-	  _putchar('\n');
-	}
-    }
-  else if (length <= 0)
-    {
-      _putchar('\n');
-    }
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < col; b++)
+{
+_putchar(' ');
+}
+columns--;
+for (c = 0; c < (a + 1); c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else if (length <= 0)
+{
+_putchar('\n');
+}
 }
