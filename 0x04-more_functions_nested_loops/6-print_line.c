8fafa2231e8e43a2c428cf2212f00d69dcc4a1aa
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-12 07:10:48 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * a function that prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ */
+
+void print_line(int n)
+{
+
+int i;
+for (i = 0; n <= n; i++)
+{
+if(i <=0)
+_putchar('\n');
+_putchar('_');
+}
+_putchar('\n');
+}
+}
