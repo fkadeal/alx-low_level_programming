09c263dd173047a6865a99c724c8463ec939961a
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-12 06:11:12 -0800

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * a function that prints 10 times the numbers, 
+ * a function that prints 10 times the numbers,
  * from 0 to 14, followed by a new line.
  */
 
@@ -14,7 +14,14 @@ for (n = 1; n <= 10; n++)
 {
 for (i = 0; i <= 14; i++)
 {
+
+if(i <=9 )
+{
 _putchar(i+ '0');
+}else{
+_putchar('0' + i / 10);
+_putchar('0' + i % 10);
+}
 }
 _putchar('\n');
 }
