823c37636a81d0b3f0444a276d98b0d360051331
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-12 07:33:09 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,17 @@ void print_line(int n)
 {
 
 int i;
+  if(i <=0)
+{
+_putchar('\n');
+}
+else 
+{
 for (i = 0; i <= n; i++)
 {
-if(i <=0)
-{
-_putchar('\n');
-}
-  else
-{
 _putchar('_');
+{
+
 }
 _putchar('\n');
-}
-  
 }
