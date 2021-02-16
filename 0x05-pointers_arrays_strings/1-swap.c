e0fda44f6e08ad4a7f8e68f5e478068870623fea
fkadeal <infkadeal@gmail.com>
2021-02-16 06:18:56 -0800

    modifiy for betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
- *a function that swap given int values 
+ *a function that swap given int value
  *by etbaba
  **/
 void swap_int(int *a, int *b)
+/*swap_int : function to swap values of 'a' and 'b' */
 {
 int swap;
 swap = *a;
