b612ed501d2c463cc84600eb682d2408d01a374b
fkadeal <infkadeal@gmail.com>
2021-02-17 07:51:58 -0800

    modifiy from peer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,43 @@
 #include "holberton.h"
 
 /**
- **rev_string: a fnctioin that reverses a string.
- **@s: string char *
- */
+ *  **rev_string: a fnctioin that reverses a string.
+ *   **@s: string char *
+ *    */
 
 void rev_string(char *s)
 {
-int i, j;
-char sriv[10];
-j = 0;
-for (i = 8 ; i >= 0 ; i--)
-{
-sriv[j] = s[i];
-j++;
+	
+	int x, i;
+	x = 0;
+	char *t = s, *d = s, p;
+	
+	while (s[x])
+		x++;
+	
+	for (i = 0; i < x - 1; i++)
+	{
+		d++;
 }
-for (i = 0 ; i <= 10 ; i++)
-{
-s[i] = sriv[i];
-}
+	
+	for (i = 0; i < x--; i++)
+	{
+		p = *d;
+		*d = *t;
+		*t = p;
+		t++;
+		d--;
+	}/*
+	int i, j;
+	char sriv[10];
+	j = 0;
+	for (i = 8 ; i >= 0 ; i--)
+	{
+		sriv[j] = s[i];
+		j++;
+	}
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		s[i] = sriv[i];
+	}*/
 }
