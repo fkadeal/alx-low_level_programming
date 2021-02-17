7665ee871ffbc0c727ceefc467bd28e8e3483b93
fkadeal <infkadeal@gmail.com>
2021-02-17 05:10:03 -0800

    final modify

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,30 +1,18 @@
 #include "holberton.h"
+
+/*
+ *print_rev - a function to make string reverse
+ */
+
 void print_rev(char *s)
 {
-int count = 0;                                                                                                                                         while (*s != '\0')                                                                                                                                     {                                                                                                                                                              count++;                                                                                                                                               s++;                                                                                                                                           }     
-int length, c;
-   char *begin, *end, temp;
-    
-      length = count;
-         begin  = s;
-	    end    = s;
-	     
-	       for (c = 0; c < length - 1; c++)
-		             end++;
-for (c = 0; c < length/2; c++)
-{        
-temp   = *end;
-*end   = *begin;
-*begin = temp;
-begin++;
-end--;
-}
-
-while (*s != '\0')
+int qcount = 0;
+while (s[qcount])
 {
-	        _putchar(*s);
-		        s++;
+qcount++;
 }
+while (--qcount >= 0)
+_putchar(s[qcount]);
+
 _putchar('\n');
 }
-
