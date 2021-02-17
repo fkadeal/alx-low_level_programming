858b0e3016497deed63bb279410c1cca1358ee46
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-02-17 13:04:27 -0800

    mod

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,13 +7,15 @@
 
 int _atoi(char *s)
 {
-int sign = 1, i;
+int x;
+  int i;
+  x = 1;
 unsigned int num = 0;
 
 for (i = 0; s[i]; i++)
 {
 if (s[i] == '-')
-sign *= -1;
+x *= -1;
 
 else if (s[i] >= '0' && s[i] <= '9')
 num = (num * 10) + (s[i] - '0');
@@ -22,5 +24,5 @@ else if (num > 0)
 break;
 }
 
-return (num *sign);
+return (num *x);
 }
