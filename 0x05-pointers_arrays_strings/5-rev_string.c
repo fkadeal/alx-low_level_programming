d49c6044b21398f4f9b5c941cba6bf6885078500
fkadeal <infkadeal@gmail.com>
2021-02-17 06:58:46 -0800

    revierce function

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ **rev_string: a fnctioin that reverses a string.
+ **@s: string char *
+ */
+
+void rev_string(char *s)
+{
+int i, j, x;
+char sriv[10];
+j = 0;
+for (i = 8 ; i >= 0 ; i--)
+{
+sriv[j] = s[i];
+j++;
+}
+for (i = 0 ; i <= 10 ; i++)
+{
+s[i] = sriv[i];
+}
+}
