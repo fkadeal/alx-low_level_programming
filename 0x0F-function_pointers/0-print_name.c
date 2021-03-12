0e94fbb7458ca3fa40ef937aebc7a5cab746451c
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-03-12 11:22:40 -0800

    final push

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to string
+ * @f: pointer to a function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f)
+f(name);
+}
