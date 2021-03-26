7de95c7c1dddad8236267de170c8f06e25cfe92f
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-03-26 10:15:19 -0700

    blow-Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include "holberton.h"
-/**
+
  *binary_to_uint - this function that convert to binary
  *@b: pointer to unsignid int
  *Return: returns converted unsignid int
- */
+
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int result = 0, i = 0;
@@ -21,4 +21,39 @@ while (b[i])
 	i++;
 	}
 return (result);
+}
+*/
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts binary number to an unsigned int.
+ * @b: binary string.
+ * Return: unsigned int, converted number.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int total;
+
+if (b == NULL)
+return (0);
+
+total = 0;
+while (*b)
+{
+total <<= 1;
+if (*b == '1')
+total += 1;
+else if (*b == '0')
+{
+b++;
+continue;
+}
+else
+return (0);
+b++;
+}
+return (total);
+
 }
