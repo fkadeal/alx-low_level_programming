e5316f6d202b9bb27a356fea957086116e5c88b8
fkadeal <infkadeal@gmail.com>
2021-03-26 10:56:01 -0700

    final push sry

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,29 @@
 #include "holberton.h"
+
 /**
- *binary_to_uint - this function that convert to binary
- *@b: pointer to unsignid int
- *Return: returns converted unsignid int
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result = 0, i = 0;
-if (!b)
-	return (0);
-while (b[i])
-{
-	if (!(b[i] == '0' || b[i] == '1'))
-	return (0);
-	result <<= 1;
-	if (b[i] == '1')
-	result += 1;
+	int i;
+	unsigned int num;
 
-	i++;
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 	}
-return (result);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,17 +1,13 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef _FL_HOLBERTON_H_
+#define _FL_HOLBERTON_H_
+#include <stdio.h>
+#include <stdlib.h>
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
+int get_endianness(void);
 #endif
