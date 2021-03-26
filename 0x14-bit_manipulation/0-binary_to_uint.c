cf578ba265e40f20b3c5c737af9a1f61892bee7a
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-03-26 10:51:50 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,59 +1,29 @@
-/*#include <stdio.h>
-#include "holberton.h"
-
- *binary_to_uint - this function that convert to binary
- *@b: pointer to unsignid int
- *Return: returns converted unsignid int
-
-unsigned int binary_to_uint(const char *b)
-{
-unsigned int result = 0, i = 0;
-if (!b)
-	return (0);
-while (b[i])
-{
-	if (!(b[i] == '0' || b[i] == '1'))
-	return (0);
-	result <<= 1;
-	if (b[i] == '1')
-	result += 1;
-
-	i++;
-	}
-return (result);
-}
-*/
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * binary_to_uint - converts binary number to an unsigned int.
- * @b: binary string.
- * Return: unsigned int, converted number.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-int total;
+	int i;
+	unsigned int num;
 
-if (b == NULL)
-return (0);
-
-total = 0;
-while (*b)
-{
-total <<= 1;
-if (*b == '1')
-total += 1;
-else if (*b == '0')
-{
-b++;
-continue;
-}
-else
-return (0);
-b++;
-}
-return (total);
-
+	num = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
