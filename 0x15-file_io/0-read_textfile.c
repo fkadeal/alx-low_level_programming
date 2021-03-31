01693bfb6ab7ecfe08d8d0f8dae2e1cae81a3e3a
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-03-30 18:22:21 -0700

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,9 +5,30 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+  int file, fd;
+  char *buffer;
   if (filename == NULL )
   {
     return (0);
 }
+  buffer = malloc(sizeof(char) * letter);
+  if (buffer == NULL)
+  {
+    return (0);
+  }
+  
+  file = open(filename,O_RDONLY);
+  if (file < 0)
+  {
+    free(buffer);
+    return (0);
+  }
+  
+  fd = read(file, buffer,letters);
+  write(STDOUT_FILENO, buffer, fd);
+  close(file);
+  
+  return (fd);
+
   
 }
