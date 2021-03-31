93c693a04ca0dce3776e3477d42b03d2af40a4b2
fkadeal <infkadeal@gmail.com>
2021-03-30 18:55:59 -0700

    fix all betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,32 @@
-#include "holberton.h"
+#include "hlberton.h"
 /**
-*ssize_t - this will read and write to posix
-*
-*/
+**read_textfile- ssize_t - this will read and write to posix
+*@filename : the file to be inserted when program run
+*@letters : number of chars to be readn and writen
+*read_textfile : function that reads file
+*Return: actual number of letters it could read and print
+**/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int file, fd;
-  char *buffer;
-  if (filename == NULL )
-  {
-    return (0);
+int file, fd;
+char *buffer;
+if (filename == NULL)
+{
+return (0);
 }
-  buffer = malloc(sizeof(char) * letters);
-  if (buffer == NULL)
-  {
-    return (0);
-  }
-  
-  file = open(filename,O_RDONLY);
-  if (file < 0)
-  {
-    free(buffer);
-    return (0);
-  }
-  
-  fd = read(file, buffer,letters);
-  write(STDOUT_FILENO, buffer, fd);
-  close(file);
-  
-  return (fd);
-
-  
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
+file = open(filename, O_RDONLY);
+if (file < 0)
+{
+free(buffer);
+return (0);
+}
+fd = read(file, buffer, letters);
+write(STDOUT_FILENO, buffer, fd);
+close(file);
+return (fd);
 }
