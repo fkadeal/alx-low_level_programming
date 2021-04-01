aa3050c413a74031a50bbaafc0885e26b881f137
fkadeal <44930374+fkadeal@users.noreply.github.com>
2021-04-01 11:25:16 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	close(fd);
 	return (1);
-}/*
+}
+
+/**
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, opn;
@@ -46,4 +48,5 @@ return (-1);
 }
 close(opn);
 return (1);
-}*/
+}
+**/
